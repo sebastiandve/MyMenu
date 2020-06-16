6d07aa6459765ccd52c2d3ba2b21ed591f01ecc2
Sebastian <>
2020-06-16 11:53:01 +0200

    update menu link

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import Flask, redirect
 app = Flask(__name__)
 
 # Programatically create new routes
+# https://production-my-menu.herokuapp.com/yanncouvreur
 @app.route('/yanncouvreur')
 def redirect_to_menu():
     # This function should find the latest menu available
 
-    return redirect("https://all-my-menus.s3.eu-west-2.amazonaws.com/yanncouvreur/uncertainty.pdf")
+    return redirect("https://all-my-menus.s3.eu-west-2.amazonaws.com/yanncouvreur/Menu+YCH+COVID.pdf")
 
 
 if __name__ == '__main__':
