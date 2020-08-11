2bc547c30a0a8198bbb1c0fcbfc131bd56dd2027
Sebastian <>
2020-08-10 20:06:27 -1000

    update menu link

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def redirect_to_menu():
     # This function should find the latest menu available
 
-    return redirect("https://all-my-menus.s3.eu-west-2.amazonaws.com/yanncouvreur/Menu+YCH+July+V.3.pdf")
+    return redirect("https://all-my-menus.s3.eu-west-2.amazonaws.com/yanncouvreur/Menu+YCH.pdf")
 
 
 if __name__ == '__main__':
